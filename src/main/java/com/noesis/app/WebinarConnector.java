3a540ac81693748af1f6e62a26231fe1de68012e
Shaelyn Watson <shaelynjoy@gmail.com>
2014-06-05 14:24:40 -0500

    wrote comment less blank

--- src/main/java/com/noesis/app/WebinarConnector.java
+++ src/main/java/com/noesis/app/WebinarConnector.java
@@ -1,9 +1,13 @@
 package com.noesis.app;
 
+/*
+ * Registers a user for a webinar on GoToWebinar using given information
+ */
+
 public class WebinarConnector {
 	
 	public WebinarConnector() {
-		System.out.println("Heyyyy");
+		System.out.println("Webinar Constructer");
 	}
 	
 
