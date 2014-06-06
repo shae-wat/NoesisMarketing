e4acdf8ac91dd95d06be929ab09293cc5290a5ec
Shaelyn Watson <shaelynjoy@gmail.com>
2014-06-06 14:43:05 -0500

    implemented method from Utils.java

--- src/main/java/com/noesis/app/WebinarConnector.java
+++ src/main/java/com/noesis/app/WebinarConnector.java
@@ -12,6 +12,14 @@ public class WebinarConnector {
 		String contactURL = "http://www.iflscience.com/brain/five-cool-ways-trick-your-brain";
 		
 		 // /rest/{service name}  
+		try {
+			Utils.readFromURL(contactURL);
+			System.out.println("Try successful");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("try unsuccessful");
+		}
 	}
 	
 
