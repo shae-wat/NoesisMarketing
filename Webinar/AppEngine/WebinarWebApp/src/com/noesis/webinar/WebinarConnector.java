7c401ab307d7f34e4d94342f69a9d9200dd2ef46
scottallencarter <scottcarter@Scott-Carters-MacBook-Pro.local>
2014-07-22 10:16:59 -0500

    Basic working webinar api auth using AppEngine

---
+++ Webinar/AppEngine/WebinarWebApp/src/com/noesis/webinar/WebinarConnector.java
@@ -0,0 +1,42 @@
+package com.noesis.webinar;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+import com.google.gson.Gson;
+
+public class WebinarConnector {
+
+	WebinarAuth wa;
+	Gson gson; 
+	
+	public WebinarConnector ()
+	{
+		try {
+		    URL url = new URL("https://api.citrixonline.com/oauth/access_token?grant_type=password&user_id=rallen@noesis.com&password=Austin2013&client_id=WUKeRBGxGEyH0gTEe2UG1ijANkaWL8Gy");
+		    BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+		    String line ="";
+		    String rline = "";
+
+		    while ((rline = reader.readLine()) != null) {
+		        line = line + rline;
+		    }
+		    reader.close();
+		    
+		    gson = new Gson();
+		    wa = gson.fromJson(line, WebinarAuth.class);
+			System.out.println("GET auth data = " + line);
+			System.out.println("access token = " + wa.getAccess_token());
+			System.out.println("organizer key = " + wa.getOrganizer_key());
+
+		} catch (MalformedURLException e) {
+		    // ...
+		} catch (IOException e) {
+		    // ...
+		}
+	}
+	
+}
