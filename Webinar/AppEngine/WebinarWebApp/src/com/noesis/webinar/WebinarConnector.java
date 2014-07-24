dc53e89a36a539a09e3fa1014c979f787e2069a9
Shaelyn Watson <shaelynjoy@gmail.com>
2014-07-24 12:23:11 -0500

    working header auth

--- Webinar/AppEngine/WebinarWebApp/src/com/noesis/webinar/WebinarConnector.java
+++ Webinar/AppEngine/WebinarWebApp/src/com/noesis/webinar/WebinarConnector.java
@@ -49,20 +49,7 @@ public class WebinarConnector {
 		URL url = new URL("https://api.citrixonline.com/G2W/rest/organizers/" + wa.getOrganizer_key() + "/upcomingWebinars");
 		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 		connection.setRequestMethod("GET");
-		
-		String oa = "OAuth oauth_token";
-		connection.setRequestProperty("charset", "utf-8" );
-		connection.addRequestProperty(oa, wa.getAccess_token());  //addRequestProperty
-//		
-		Map<String,List<String>> props = new HashMap<String,List<String>>();
-		props = connection.getRequestProperties();
-		for (Entry<String, List<String>> entry : props.entrySet()){
-			System.out.println("key = " + entry.getKey());
-			for (String li : entry.getValue()){
-				System.out.println(li);
-			}
-			System.out.println("\n");
-		}
+		connection.setRequestProperty("Authorization", "OAuth oauth_token="+wa.getAccess_token());
 		
 		connection.connect();
 		
@@ -72,11 +59,6 @@ public class WebinarConnector {
 		
 		BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 		while((line = in.readLine()) != null)
-		{
-			System.out.println(line);
-		}
-		BufferedReader err = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
-		while((line = err.readLine()) != null)
 		{
 			System.out.println(line);
 		}
