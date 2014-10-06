f6e7f24d34978307953a0f955932a488d7296322
Shaelyn Watson <shaelynjoy@gmail.com>
2014-10-06 12:34:44 -0500

    optInQ sets to true false

--- FormValidation/formValidation-app/src/main/java/com/formValidation/app/App.java
+++ FormValidation/formValidation-app/src/main/java/com/formValidation/app/App.java
@@ -15,18 +15,34 @@ public class App
     public static void main( String[] args ) throws IOException
     {
     	List<String> urlList = new ArrayList<String>();
+    	boolean optInQ = false;
     	urlList.add("https://www.noesisenergy.com/site/content/energy-efficient-lighting-explained-0?");
     	urlList.add("https://www.noesisenergy.com/site/content/energy-project-finance-cheat-sheet");
     	
+    	/*  How many fields are on the form?  */
     	for(String url : urlList){
     	 Document doc = Jsoup.connect(url).get();
          Elements inputs = doc.select("input");
+         System.out.println("\n========\nForm url: " + url + "\n");
          for(Element input : inputs) {
-             System.out.println(input.attr("name"));
-             System.out.println(input.attr("value"));
+             System.out.println(input.attr("name") + " = " + input.attr("value"));
+             if (input.attr("name").equals("Landing_Page_Opt_In__c")){
+            	 optInQ = true;
+             }
          }
+    	
+    	
+    	
+    	/*  Does the form include the Google Analytics fields?  */
+    	
+    	/*  Does the page include the Google Analytics script?  */
+    	
+    	/*  What is the value of the hidden 'source' field?  */
+    	
+    	/*  Is there an 'opt in' question on the form? What is the text of the opt in question?  */
+         System.out.println("\nIs there an 'opt in' question on the form? " + optInQ);
+         
     	}
-    	 
     	System.out.println( "\nForm Validator!" );
     }
 }
