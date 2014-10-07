3311af2faeb5f5623b82b8d38feada9c8969c1ec
Shaelyn Watson <shaelynjoy@gmail.com>
2014-10-07 11:15:21 -0500

    printing ng-inits

--- FormValidation/formValidation-app/src/main/java/com/formValidation/app/App.java
+++ FormValidation/formValidation-app/src/main/java/com/formValidation/app/App.java
@@ -25,7 +25,7 @@ public class App
          Elements inputs = doc.select("input");
          System.out.println("\n========\nForm url: " + url + "\n");
          for(Element input : inputs) {
-             System.out.println(input.attr("name") + " = " + input.attr("value"));
+             System.out.println(input.attr("ng-init"));
              if (input.attr("name").equals("Landing_Page_Opt_In__c")){
             	 optInQ = true;
              }
@@ -33,7 +33,7 @@ public class App
          }
          Elements otros = doc.select("p");
          for (Element p : otros){
-        	 System.out.println(p.select("p"));
+        	 System.out.println(p.select("p[style=color:#FFFFFF]"));
          }
     	
     	
