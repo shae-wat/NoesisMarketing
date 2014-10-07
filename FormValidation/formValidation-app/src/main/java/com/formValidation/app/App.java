6f55a7e6f903a68c15e34fb369d26c8d3b9408c3
Shaelyn Watson <shaelynjoy@gmail.com>
2014-10-07 14:44:51 -0500

    refactored form val

--- FormValidation/formValidation-app/src/main/java/com/formValidation/app/App.java
+++ FormValidation/formValidation-app/src/main/java/com/formValidation/app/App.java
@@ -15,40 +15,52 @@ public class App
     public static void main( String[] args ) throws IOException
     {
     	List<String> urlList = new ArrayList<String>();
-    	boolean optInQ = false;
     	urlList.add("https://www.noesisenergy.com/site/content/energy-efficient-lighting-explained-0?");
     	urlList.add("https://www.noesisenergy.com/site/content/energy-project-finance-cheat-sheet");
     	
-    	/*  How many fields are on the form?  */
+    	boolean optInQ = false;
+    	List<String> fieldList = new ArrayList<String>();
+    	
     	for(String url : urlList){
     	 Document doc = Jsoup.connect(url).get();
          Elements inputs = doc.select("input");
          System.out.println("\n========\nForm url: " + url + "\n");
+         
          for(Element input : inputs) {
+        	 System.out.println(input.attr("name"));
              System.out.println(input.attr("ng-init"));
-             if (input.attr("name").equals("Landing_Page_Opt_In__c")){
-            	 optInQ = true;
-             }
+             
+             fieldList.add(input.attr("name"));
+             fieldList.add(input.attr("ng-init"));
+             
+
              
          }
+         
+    	
+         /*  How many fields are on the form?  */
+         
+    	
+    	/*  Does the form include the Google Analytics fields?  */
+    	
+    	
+    	/*  What is the value of the hidden 'source' field?  */
+         
+    	
+    	/*  Is there an 'opt in' question on the form? What is the text of the opt in question?  */
+         if(fieldList.contains("Landing_Page_Opt_In__c")){
+        	 optInQ = true;
+         }
+         System.out.println("\nIs there an 'opt in' question on the form? " + optInQ);
+         
          Elements otros = doc.select("p");
          for (Element p : otros){
         	 System.out.println(p.select("p[style=color:#FFFFFF]"));
          }
-    	
-    	
-    	
-    	/*  Does the form include the Google Analytics fields?  */
-    	
-    	/*  Does the page include the Google Analytics script?  */
-    	
-    	/*  What is the value of the hidden 'source' field?  */
-    	
-    	/*  Is there an 'opt in' question on the form? What is the text of the opt in question?  */
-         System.out.println("\nIs there an 'opt in' question on the form? " + optInQ);
          
          
+        // end loop over urls 
     	}
-    	System.out.println( "\nForm Validator!" );
+    	System.out.println( "\n======Form validation complete" );
     }
 }
