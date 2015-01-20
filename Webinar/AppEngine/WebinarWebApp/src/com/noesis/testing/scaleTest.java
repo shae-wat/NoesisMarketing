523a337bc6eac32eaefaa42b6d6f9ddff821277f
Shaelyn-Watson <shaelynjoy@gmail.com>
2015-01-20 11:54:18 -0800

    rough draft scale testing java file

---
+++ Webinar/AppEngine/WebinarWebApp/src/com/noesis/testing/scaleTest.java
@@ -0,0 +1,17 @@
+package com.noesis.testing;
+
+public class scaleTest{
+	
+	public scaleTest(){
+		//webinar connector
+		//registration for a webinar
+	}
+	
+	public void runScaleTests(int numTests){
+		//for numTests
+			//create new scaleTest instance
+			//if fail, freak out
+		//else return successful scale test message
+	}
+	
+}
